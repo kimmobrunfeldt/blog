f12a89eec1193c132ea2378008df588c86bf4a22
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2021-03-01 00:21:07 +0200

    Fix layouts and add tslib dep

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -4,6 +4,7 @@ import { Icon } from "@iconify/react";
 import arrowLeftOutline from "@iconify/icons-teenyicons/left-small-outline";
 import { isPostPage, PostMetadata, SiteData } from "src/types/siteData";
 import { Footer } from "src/components/Footer";
+import { ContentWrapper } from "src/components/ContentWrapper";
 import { H } from "src/components/H";
 import { Link } from "src/components/Link";
 import { NavBar } from "src/components/NavBar";
@@ -39,50 +40,58 @@ function findRelated(
 }
 
 export function PostLayout(props: Props): JSX.Element {
-  const mdxContainerCls = "mdx row-start-0 col-span-4 max-w-xl";
+  const mdxContainerCls =
+    "mdx row-start-2 md:row-start-1 col-span-12 col-start-1 md:col-start-5 md:col-span-6 max-w-md";
 
   return (
     <div className={`grid grid-rows-layout min-h-full gap-global`}>
       <NavBar siteData={props.siteData} pageData={props.data} />
-      <main className={`grid grid-cols-12 grid-rows-post gap-global`}>
-        <div className="row-start-0 col-start-3 col-span-2 text-sm">
-          <div className="text-gray-6 font-bold text">
-            {formatPostDate(props.data.createdAt)}
+      <ContentWrapper>
+        <main
+          className={`grid grid-cols-12 grid-rows-post-sm md:grid-rows-post gap-global`}
+        >
+          <div className="row-start-1 text-sm col-start-1 col-span-12 md:col-start-3 md:col-span-2 mb-10 md:mb-0">
+            <div className="text-gray-6 font-bold text">
+              {formatPostDate(props.data.createdAt)}
+            </div>
+            <div className="text-gray-5">
+              {kFormatter(props.data.charCount)} chars
+            </div>
+            <div className="text-gray-5 mt-4 italic text-xs">
+              {props.data.tags.join(", ")}
+            </div>
+
+            {/* It's ~OK to have this visible for screenreaders */}
+            <Link
+              className="hidden md:visible md:inline-block pt-7"
+              href="/posts"
+            >
+              <Icon className="inline-block" icon={arrowLeftOutline} /> Back to
+              posts
+            </Link>
           </div>
-          <div className="text-gray-5">
-            {kFormatter(props.data.charCount)} chars
+
+          {"children" in props ? (
+            <article className={mdxContainerCls}>
+              <H>{props.data.title}</H>
+              {props.children}
+            </article>
+          ) : (
+            <article className={mdxContainerCls}>
+              <H>{props.data.title}</H>
+              {/* Frontend hydrate adds an additional div inside article, we must match it */}
+              <div dangerouslySetInnerHTML={{ __html: props.html }} />
+            </article>
+          )}
+
+          <div className="row-start-3 md:row-start-2 col-start-1 col-span-12 md:col-start-5 md:col-span-4">
+            <Link className="inline-block pt-5" href="/posts">
+              <Icon className="inline-block" icon={arrowLeftOutline} /> Back to
+              all posts
+            </Link>
           </div>
-          <div className="text-gray-5 mt-4 italic text-xs">
-            {props.data.tags.join(", ")}
-          </div>
-
-          <Link className="inline-block pt-7" href="/posts">
-            <Icon className="inline-block" icon={arrowLeftOutline} /> Back to
-            posts
-          </Link>
-        </div>
-
-        {"children" in props ? (
-          <article className={mdxContainerCls}>
-            <H>{props.data.title}</H>
-            {props.children}
-          </article>
-        ) : (
-          <article className={mdxContainerCls}>
-            <H>{props.data.title}</H>
-            {/* Frontend hydrate adds an additional div inside article, we must match it */}
-            <div dangerouslySetInnerHTML={{ __html: props.html }} />
-          </article>
-        )}
-
-        <div className="row-start-0 col-start-5 col-span-4 pt-10">
-          <Link className="inline-block pt-5" href="/posts">
-            <Icon className="inline-block" icon={arrowLeftOutline} /> Back to
-            all posts
-          </Link>
-        </div>
-      </main>
-
+        </main>
+      </ContentWrapper>
       <Footer />
     </div>
   );
