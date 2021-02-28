c273d55752fdcc98f50f2df1f3a487860674528f
Kimmo Brunfeldt <1232405+kimmobrunfeldt@users.noreply.github.com>
2021-02-28 23:41:21 +0200

    Add minification and html validation for prod build

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,18 @@
+const plugins = [
+  require("postcss-import"),
+  require("tailwindcss"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push(
+    require("cssnano")({
+      preset: "default",
+    })
+  );
+}
+
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("postcss-nested"),
-    require("autoprefixer"),
-  ],
+  plugins,
 };
