3e2b75967d0eb9e6fb7eee85b1a1992a3a9ba847
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2021-03-15 23:58:28 +0200

    Improve dark mode smartness when user switches os preference

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useLocalStorage } from "src/util/storage";
+import { addMediaListener, removeMediaListener } from "src/util/site";
+import { removeKey, useLocalStorage } from "src/util/storage";
 
 type Context = {
   theme: "light" | "dark";
@@ -16,22 +17,43 @@ const context = {
 };
 export const AppContext = React.createContext<Context>(context);
 
-export const App = ({ children }: Props) => {
-  const [localStorageTheme, setLocalStorageTheme] = useLocalStorage("theme");
+function setThemeClass(theme: Context["theme"]): void {
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}
 
-  const osPreference = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
+export const App = ({ children }: Props) => {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? ("dark" as const)
+    : ("light" as const);
+
+  const [osPreference, setOsPreference] = React.useState(prefersDark);
+  const [localStorageTheme, setLocalStorageTheme] = useLocalStorage("theme");
 
   const theme = localStorageTheme === null ? osPreference : localStorageTheme;
 
   React.useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [localStorageTheme]);
+    setThemeClass(theme);
+  }, [theme]);
+
+  // Change theme immediately if user switches their OS preference and
+  // the theme hasn't been overridden in local storage
+  React.useEffect(() => {
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const cb = (event: MediaQueryListEvent) => {
+      // Remove key
+      setLocalStorageTheme(null);
+      setOsPreference(event.matches ? "dark" : "light");
+    };
+
+    addMediaListener(darkQuery, cb);
+    return () => {
+      removeMediaListener(darkQuery, cb);
+    };
+  }, []);
 
   return (
     <AppContext.Provider value={{ theme, setTheme: setLocalStorageTheme }}>
