69be653d57a6b3418746ee652b705c3ccf9f1ca0
Kimmo Brunfeldt <kimmo@swarmia.com>
2021-08-18 22:17:06 +0300

    Move build scripts into separate files and use zx to avoid bash pitfalls

---
+++ tools/transpilePcss.mjs
@@ -0,0 +1,17 @@
+import path from "path";
+import glob from "glob";
+import { promisify } from "util";
+
+const globAsync = promisify(glob);
+
+const files = await globAsync("output/*.pcss");
+
+for (let i = 0; i < files.length; ++i) {
+  const file = files[i];
+  const dirName = path.dirname(file);
+  const withoutExt = path.basename(file, path.extname(file));
+  const basePath = path.join(dirName, withoutExt);
+
+  await $`NODE_ENV=production postcss "${basePath}.pcss" -o "${basePath}.css" --verbose`;
+  await $`rm "${basePath}.pcss"`;
+}
