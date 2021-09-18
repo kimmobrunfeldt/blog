a7dffb7f8649d1373fbb65da8c302f41d48f32ee
Kimmo Brunfeldt <1232405+kimmobrunfeldt@users.noreply.github.com>
2021-09-18 22:59:08 +0300

    Add icons to mdx

--- src/components/MDXRemote.tsx
+++ src/components/MDXRemote.tsx
@@ -4,6 +4,16 @@ import * as components from "src/components";
 import { info, success, warning, error } from "src/components/Toast";
 import { components as mdxComponents } from "src/mdxComponents";
 import { PostContext } from "src/components";
+import { Icon } from "@iconify/react";
+import tickOutlineIcon from "@iconify/icons-teenyicons/tick-outline";
+import xOutlineIcon from "@iconify/icons-teenyicons/x-outline";
+import xSmallOutlineIcon from "@iconify/icons-teenyicons/x-small-outline";
+
+const icons = {
+  tickOutlineIcon,
+  xOutlineIcon,
+  xSmallOutlineIcon,
+};
 
 export type MDXRemoteProps = {
   compiledSource: string;
@@ -14,10 +24,11 @@ export const MDXRemote = ({ compiledSource }: MDXRemoteProps) => {
 
   const mdxRemoteProps = {
     compiledSource,
-    scope: { info, success, warning, error, ...ctx },
+    scope: { info, success, warning, error, ...icons, ...ctx },
     components: {
       ...components,
       ...mdxComponents,
+      Icon,
     },
   };
 
